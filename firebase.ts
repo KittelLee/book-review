6aa6e55b179712e4e7c052d2e46384677bc2f539
Leejinuk <leejin7900@gmail.com>
2023-12-06 17:08:32 +0900

    feat: CI/CD 구축

---
+++ firebase.ts
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAu1pu4r4m_kJLEyeL7Jgc6tWz94Upzk98",
+  authDomain: "book-review-a7be9.firebaseapp.com",
+  projectId: "book-review-a7be9",
+  storageBucket: "book-review-a7be9.appspot.com",
+  messagingSenderId: "905824431279",
+  appId: "1:905824431279:web:f56fdfc06bc60dd733785a",
+  measurementId: "G-L9QXD3H138",
+};
+
+const app = initializeApp(firebaseConfig);

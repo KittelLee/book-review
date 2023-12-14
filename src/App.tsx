a0438e7cdf8aae032d5dacfe5b38b1a9ba5d5e41
withsjb <withsjb1@gmail.com>
2023-12-14 15:25:09 +0900

    fix: board 버그 수정2

--- src/App.tsx
+++ src/App.tsx
@@ -5,20 +5,14 @@ import BackgroundImg from "./assets/images/deco.png";
 import Home from "./pages/Home";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
-import Home from "./pages/Home";
 import Login from "./pages/Login";
-import BackgroundImg from "./assets/images/deco.png";
 import Board from "./pages/Board";
 import BoardWrite from "./components/Board/BoardWrite";
 import Boarddetail from "./components/Board/BoardDetail";
 import MyPage from "./pages/MyPage";
 import List from "./pages/List";
 import Search from "./pages/Search";
-import Board from "./pages/Board";
 
-
-  
-  
 const GlobalStyle = createGlobalStyle`
   body, html {
     margin: 0;
@@ -41,12 +35,12 @@ function App() {
         <Route path="/Search" element={<Search />} /> 
         */}
           <Route path="/Mypage" element={<MyPage />} />
-          
+
           <Route path="/Boardwrite" element={<BoardWrite />} />
           <Route path="/Boarddetail/:id" element={<Boarddetail />} />
           <Route path="/List" element={<List />} />
           <Route path="/Search" element={<Search />} />
-          <Route path="/Board" element={<Board />} />           
+          <Route path="/Board" element={<Board />} />
           <Route path="/Login" element={<Login />} />
         </Routes>
 

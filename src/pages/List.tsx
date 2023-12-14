7b48c2828cb25e3ef593de49591f010bd4623c9c
pozip <klee0523@naver.com>
2023-12-14 16:53:34 +0900

    feat:리스트페이지 코드 수정

--- src/pages/List.tsx
+++ src/pages/List.tsx
@@ -1,196 +1,3 @@
-// import { useState } from "react";
-// import styled from "styled-components";
-// import Modal from "react-modal";
-// import Review from "../components/Modal/Review";
-// import CatImg from "../assets/images/test5.jpg";
-
-// const customStyles = {
-//   content: {
-//     top: "50%",
-//     left: "50%",
-//     right: "auto",
-//     bottom: "auto",
-//     marginRight: "-50%",
-//     transform: "translate(-50%, -50%)",
-//     height: "80%",
-//     width: "80%",
-//   },
-// };
-
-// Modal.setAppElement("#root");
-
-// function List() {
-//   const [modalIsOpen, setModalIsOpen] = useState(false);
-
-//   function openModal() {
-//     setModalIsOpen(true);
-//   }
-
-//   function closeModal() {
-//     setModalIsOpen(false);
-//   }
-
-//   return (
-//     <ListWrap>
-//       <Book src={CatImg} onClick={openModal} />
-//       <div>
-//         <p>책제목:</p>
-//         <p>저자:</p>
-//         <p>출판사:</p>
-//       </div>
-//       <Modal
-//         isOpen={modalIsOpen}
-//         onRequestClose={closeModal}
-//         style={customStyles}
-//       >
-//         <Review />
-//         <CloseBtn>
-//           <button onClick={closeModal}>X</button>
-//         </CloseBtn>
-//       </Modal>
-//       <Book src={CatImg} onClick={openModal} />
-//       <Book src={CatImg} onClick={openModal} />
-//       <Book src={CatImg} onClick={openModal} />
-//     </ListWrap>
-//   );
-// }
-
-// export default List;
-
-// const ListWrap = styled.div`
-//   // img {
-//   //   height: 300px;
-//   //   margin: 180px;
-//   //   cursor: pointer;
-//   //   // float: left;}
-//   `
-// ;
-
-// const Book = styled.img`
-//   img {
-//     height: 300px;
-//     margin: 180px;
-//     cursor: pointer;
-//     // float: left;
-//   }
-//     `
-
-// const CloseBtn = styled.img`
-//   position: absolute;
-//   top: 10px;
-//   right: 10px;
-//   button {
-//     border: none;
-//     background-color: #fff;
-//     font-size: 1.2rem;
-//     &:hover {
-//       cursor: pointer;
-//       color: red;
-//     }
-//   }`
-// ;
-
-// import { useState } from "react";
-// import styled from "styled-components";
-// import Modal from "react-modal";
-// import Review from "../components/Modal/Review";
-// import CatImg from "../assets/images/test5.jpg";
-
-// const customStyles = {
-//   content: {
-//     top: "50%",
-//     left: "50%",
-//     right: "auto",
-//     bottom: "auto",
-//     marginRight: "-50%",
-//     transform: "translate(-50%, -50%)",
-//     height: "80%",
-//     width: "80%",
-//   },
-// };
-
-// Modal.setAppElement("#root");
-
-// // New BookInfo component to display book details
-// const BookInfo = () => (
-//   <InfoContainer>
-//     <div>Title: Sample Title</div>
-//     <div>Author: Sample Author</div>
-//     <div>Publisher: Sample Publisher</div>
-//   </InfoContainer>
-// );
-
-// function List() {
-//   const [modalIsOpen, setModalIsOpen] = useState(false);
-
-//   function openModal() {
-//     setModalIsOpen(true);
-//   }
-
-//   function closeModal() {
-//     setModalIsOpen(false);
-//   }
-
-//   return (
-//     <ListWrap>
-//       {/* Example of one image with book information */}
-//       <ImageContainer>
-//         <img src={CatImg} onClick={openModal} />
-//         <BookInfo />
-//       </ImageContainer>
-
-//       {/* Add more images with book information as needed */}
-//       <ImageContainer>
-//         <img src={CatImg} onClick={openModal} />
-//         <BookInfo />
-//       </ImageContainer>
-
-//       {/* Modal setup */}
-//       <Modal
-//         isOpen={modalIsOpen}
-//         onRequestClose={closeModal}
-//         style={customStyles}
-//       >
-//         <Review />
-//         <CloseBtn>
-//           <button onClick={closeModal}>X</button>
-//         </CloseBtn>
-//       </Modal>
-//     </ListWrap>
-//   );
-// }
-
-// export default List;
-
-// const ListWrap = styled.div`
-//   /* Add styling for the container if needed */
-// `;
-
-// const ImageContainer = styled.div`
-//   position: relative;
-//   margin: 20px;
-
-//   img {
-//     height: 300px;
-//     cursor: pointer;
-//   }
-// `;
-
-// const InfoContainer = styled.div`
-//   position: absolute;
-//   bottom: 0;
-//   left: 50%;
-//   transform: translateX(-50%);
-//   background: rgba(255, 255, 255, 0.8);
-//   padding: 10px;
-//   text-align: center;
-//   width: 100%;
-// `;
-
-// const CloseBtn = styled.div`
-//   /* Your styling for the close button */
-// `;
-
 import { useState } from "react";
 import styled from "styled-components";
 import Modal from "react-modal";
@@ -223,55 +30,105 @@ function List() {
     setModalIsOpen(false);
   }
 
-  return (
-    <ListWrap>
-      <Book src={CatImg} onClick={openModal}>
-        
-      </Book>
-      <div>
-          <p>책제목:</p>
-          <p>저자:</p>
-          <p>출판사:</p>
-        </div>
-      <Modal
-        isOpen={modalIsOpen}
-        onRequestClose={closeModal}
-        style={customStyles}
-      >
-        <Review />
-        <CloseBtn>
-          <button onClick={closeModal}>X</button>
-        </CloseBtn>
-      </Modal>
-      <Book src={CatImg} onClick={openModal}/>
-      <Book src={CatImg} onClick={openModal}/>
-      <Book src={CatImg} onClick={openModal}/>
-    </ListWrap>
-  );
+return (
+  <ListWrap>
+    <Bookdiv>
+    <BookContainer onClick={openModal}>
+        <Book src={CatImg} />
+        <BookInfo>
+          <p>책제목: 고양이</p>
+          <p>저자: 집사</p>
+          <p>출판사: 집사협회</p>
+          <p>가격: 1004원</p>
+        </BookInfo>
+      </BookContainer>
+
+      <BookContainer>
+        <Book src={CatImg} onClick={openModal} />
+        <BookInfo>
+          <p>책제목: 고양이</p>
+          <p>저자: 집사</p>
+          <p>출판사: 집사협회</p>
+          <p>가격: 1004원</p>
+        </BookInfo>
+      </BookContainer>
+
+      {/* <BookContainer>
+        <Book src={CatImg} onClick={openModal} />
+        <BookInfo>
+          <p>책제목: 고양이</p>
+          <p>저자: 집사</p>
+          <p>출판사: 집사협회</p>
+          <p>가격: 1004원</p>
+        </BookInfo>
+      </BookContainer>
+
+      <BookContainer>
+        <Book src={CatImg} onClick={openModal} />
+        <BookInfo>
+          <p>책제목: 고양이</p>
+          <p>저자: 집사</p>
+          <p>출판사: 집사협회</p>
+          <p>가격: 1004원</p>
+        </BookInfo>
+      </BookContainer> */}
+    </Bookdiv>
+
+    <Modal
+      isOpen={modalIsOpen}
+      onRequestClose={closeModal}
+      style={customStyles}
+    >
+      <Review />
+      <CloseBtn>
+        <button onClick={closeModal}>X</button>
+      </CloseBtn>
+    </Modal>
+  </ListWrap>
+);
 }
 
 export default List;
 
 const ListWrap = styled.div``;
 
+const Bookdiv = styled.div``;
+
+const BookContainer = styled.div`
+position: relative;
+margin: 20px;
+width: 500px;
+left: 0;
+`;
+
 const Book = styled.img`
-  height: 300px;
-  margin: 180px;
-  // padding: 100px;
-  cursor: pointer;
+height: 300px;
+cursor: pointer;
+`;
+
+const BookInfo = styled.div`
+cursor: pointer;
+// position: absolute;
+bottom: 0;
+// left: 30%;
+// transform: translateX(-50%);
+// background: rgba(255, 255, 255, 0.8);
+padding: 10px;
+text-align: center;
+// width: 10%;
 `;
 
 const CloseBtn = styled.div`
-  position: absolute;
-  top: 10px;
-  right: 10px;
-  button {
-    border: none;
-    background-color: #fff;
-    font-size: 1.2rem;
-    &:hover {
-      cursor: pointer;
-      color: red;
-    }
+position: absolute;
+top: 10px;
+right: 10px;
+button {
+  border: none;
+  background-color: #fff;
+  font-size: 1.2rem;
+  &:hover {
+    cursor: pointer;
+    color: red;
   }
+}
 `;

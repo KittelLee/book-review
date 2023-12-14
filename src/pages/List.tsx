016911b7872403ff3ddcf74c177948233814c6eb
pozip <klee0523@naver.com>
2023-12-14 15:18:33 +0900

    fix:이미지 버그수정

--- src/pages/List.tsx
+++ src/pages/List.tsx
@@ -1,7 +1,86 @@
+// import { useState } from "react";
+// import styled from "styled-components";
+// import Modal from "react-modal";
+// import Review from "../components/Modal/Review";
+
+// const customStyles = {
+//   content: {
+//     top: "50%",
+//     left: "50%",
+//     right: "auto",
+//     bottom: "auto",
+//     marginRight: "-50%",
+//     transform: "translate(-50%, -50%)",
+//     height: "80%",
+//     width: "80%",
+//   },
+// };
+
+// Modal.setAppElement("#root");
+
+// function List() {
+//   const [modalIsOpen, setModalIsOpen] = useState(false);
+
+//   function openModal() {
+//     setModalIsOpen(true);
+//   }
+
+//   function closeModal() {
+//     setModalIsOpen(false);
+//   }
+
+//   return (
+//     <ListWrap>
+//       <Book src="../../src/assets/images/test5.jpg" onClick={openModal}/>
+//       <Modal
+//         isOpen={modalIsOpen}
+//         onRequestClose={closeModal}
+//         style={customStyles}
+//       >
+//         <Review />
+//         <CloseBtn>
+//           <button onClick={closeModal}>X</button>
+//         </CloseBtn>
+//       </Modal>
+//       <Book src="../../../src/assets/images/test5.jpg" onClick={openModal}/>
+//       <Book src="../../../src/assets/images/test5.jpg" onClick={openModal}/>
+//       <Book src="../../../src/assets/images/test5.jpg" onClick={openModal}/>
+//     </ListWrap>
+//   );
+// }
+
+// export default List;
+
+// const ListWrap = styled.div``;
+
+// const Book = styled.img`
+//   height: 300px;
+//   margin: 180px;
+//   // padding: 100px;
+//   cursor: pointer;
+// `;
+
+// const CloseBtn = styled.div`
+//   position: absolute;
+//   top: 10px;
+//   right: 10px;
+//   button {
+//     border: none;
+//     background-color: #fff;
+//     font-size: 1.2rem;
+//     &:hover {
+//       cursor: pointer;
+//       color: red;
+//     }
+//   }
+// `;
+
+
 import { useState } from "react";
 import styled from "styled-components";
 import Modal from "react-modal";
 import Review from "../components/Modal/Review";
+import CatImg from "../assets/images/test5.jpg";
 
 const customStyles = {
   content: {
@@ -31,7 +110,7 @@ function List() {
 
   return (
     <ListWrap>
-      <Book src="../../../src/assets/images/test5.jpg" onClick={openModal}/>
+      <img src={CatImg} onClick={openModal} />
       <Modal
         isOpen={modalIsOpen}
         onRequestClose={closeModal}
@@ -42,25 +121,24 @@ function List() {
           <button onClick={closeModal}>X</button>
         </CloseBtn>
       </Modal>
-      <Book src="../../../src/assets/images/test5.jpg" onClick={openModal}/>
-      <Book src="../../../src/assets/images/test5.jpg" onClick={openModal}/>
-      <Book src="../../../src/assets/images/test5.jpg" onClick={openModal}/>
+      <img src={CatImg} onClick={openModal} />
+      <img src={CatImg} onClick={openModal} />
+      <img src={CatImg} onClick={openModal} />
     </ListWrap>
   );
 }
 
 export default List;
 
-const ListWrap = styled.div``;
+const ListWrap = styled.div`
+  img {
+    height: 300px;
+    margin: 180px;
+    cursor: pointer;
+  }`
+;
 
-const Book = styled.img`
-  height: 300px;
-  margin: 180px;
-  // padding: 100px;
-  cursor: pointer;
-`;
-
-const CloseBtn = styled.div`
+const CloseBtn = styled.img`
   position: absolute;
   top: 10px;
   right: 10px;
@@ -72,5 +150,5 @@ const CloseBtn = styled.div`
       cursor: pointer;
       color: red;
     }
-  }
-`;
+  }`
+;
